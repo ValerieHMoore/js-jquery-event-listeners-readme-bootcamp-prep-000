3494777bab1260400e932b6ab84d2485c4eb4f76
Valerie Moore <periwinkle17@outlook.com>
2018-05-09 20:21:36 +0000

    automatically backed up by learn

--- js/events.js
+++ js/events.js
@@ -13,7 +13,7 @@ function frameIt() {
 function pressIt() {
  $('input')on.('keydown',function(e){
    if(e.which==71);
-   alert("You have pressed the G key")
+   alert("You have pressed the G key");
  });
 }
 
